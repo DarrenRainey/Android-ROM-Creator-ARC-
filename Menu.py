1a9018f2a21d8becf9955b9d74e22633afca23ce
Darren Rainey <darren@darrenraineys.co.uk>
2017-02-15 01:43:00 +0000

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,28 @@
+def print_menu(): 
+    print 30 * "-" , "MENU" , 30 * "-"
+    print "1. LineageOS"
+    print "2. Stock Android"
+    print "3. Kernel Builder"
+    print "4. Device Information"
+    print "5. Exit"
+    print 67 * "-"
+  
+loop=True      
+  
+while loop:          ## While loop which will keep going until loop = False
+    print_menu()    ## Displays menu
+    choice = input("Enter your choice [1-5]: ")
+     
+    if choice==1:     
+        print "Not implemented yet"
+    elif choice==2:
+        print "Not implemented yet"
+    elif choice==3:
+        print "Not implemented yet"
+    elif choice==4:
+        print "Not implemented yet"
+    elif choice==5:
+	exit()
+        loop=False # This will make the while loop to end as not value of loop is set to False
+    else:
+        raw_input("Wrong option selection. Enter any key to try again..")
